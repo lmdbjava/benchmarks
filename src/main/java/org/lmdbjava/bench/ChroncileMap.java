d13cc41154991483b0e87b4f2e79fb337e678da8
Ben Alex <ben.alex@acegi.com.au>
2016-06-19 12:03:56 +1000

    Add Chroncile Map to benchmark

---
+++ src/main/java/org/lmdbjava/bench/ChroncileMap.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2016 The LmdbJava Project, http://lmdbjava.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lmdbjava.bench;
+
+import java.io.File;
+import static java.nio.ByteOrder.LITTLE_ENDIAN;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import net.openhft.chronicle.map.ChronicleMap;
+import static net.openhft.chronicle.map.ChronicleMap.of;
+import org.agrona.MutableDirectBuffer;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import static org.openjdk.jmh.annotations.Level.Invocation;
+import static org.openjdk.jmh.annotations.Level.Trial;
+import org.openjdk.jmh.annotations.Measurement;
+import static org.openjdk.jmh.annotations.Mode.SampleTime;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import static org.openjdk.jmh.annotations.Scope.Benchmark;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
+
+@OutputTimeUnit(MILLISECONDS)
+@Fork(1)
+@Warmup(iterations = 3)
+@Measurement(iterations = 3)
+@BenchmarkMode(SampleTime)
+public class ChroncileMap {
+
+  // Chroncile Map does not provide ordered key access, so no CRC/rev/prev test
+  @Benchmark
+  public void readKey(final Reader r, final Blackhole bh) throws Exception {
+    for (final int key : r.keys) {
+      if (r.intKey) {
+        r.wkb.putInt(0, key);
+      } else {
+        r.wkb.putStringWithoutLengthUtf8(0, r.padKey(key));
+      }
+      bh.consume(r.map.get(r.wkb.byteArray()));
+    }
+  }
+
+  @Benchmark
+  public void write(final Writer w, final Blackhole bh) throws Exception {
+    w.write();
+  }
+
+  @State(value = Benchmark)
+  public static class CommonChroncileMap extends Common {
+
+    ChronicleMap<byte[], byte[]> map;
+
+    /**
+     * Writable key buffer. Backed by a plain byte[] for Chroncile API ease.
+     */
+    MutableDirectBuffer wkb;
+
+    /**
+     * Writable value buffer. Backed by a plain byte[] for Chroncile API ease.
+     */
+    MutableDirectBuffer wvb;
+
+    @Override
+    public void setup() throws Exception {
+      super.setup();
+      wkb = new UnsafeBuffer(new byte[keySize]);
+      wvb = new UnsafeBuffer(new byte[valSize]);
+
+      map = of(byte[].class, byte[].class)
+          .averageKey(new byte[keySize])
+          .averageValue(new byte[valSize])
+          .entries(num)
+          .createPersistedTo(new File(tmp, "chroncile.map"));
+    }
+
+    @Override
+    public void teardown() throws Exception {
+      map.close();
+      super.teardown();
+    }
+
+    void write() throws Exception {
+      final int rndByteMax = RND_MB.length - valSize;
+      int rndByteOffset = 0;
+      for (final int key : keys) {
+        if (intKey) {
+          wkb.putInt(0, key, LITTLE_ENDIAN);
+        } else {
+          wkb.putStringWithoutLengthUtf8(0, padKey(key));
+        }
+        if (valRandom) {
+          wvb.putBytes(0, RND_MB, rndByteOffset, valSize);
+          rndByteOffset += valSize;
+          if (rndByteOffset >= rndByteMax) {
+            rndByteOffset = 0;
+          }
+        } else {
+          wvb.putInt(0, key);
+        }
+        map.put(wkb.byteArray(), wvb.byteArray());
+      }
+    }
+  }
+
+  @State(Benchmark)
+  public static class Reader extends CommonChroncileMap {
+
+    @Setup(Trial)
+    @Override
+    public void setup() throws Exception {
+      super.setup();
+      super.write();
+    }
+
+    @TearDown(Trial)
+    @Override
+    public void teardown() throws Exception {
+      super.teardown();
+    }
+  }
+
+  @State(Benchmark)
+  public static class Writer extends CommonChroncileMap {
+
+    @Setup(Invocation)
+    @Override
+    public void setup() throws Exception {
+      super.setup();
+    }
+
+    @TearDown(Invocation)
+    @Override
+    public void teardown() throws Exception {
+      super.teardown();
+    }
+  }
+
+}
